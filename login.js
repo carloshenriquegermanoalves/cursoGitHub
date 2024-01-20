d4d7a0ba929f53c8940713d8b8084dd96e52e6d3
carloshenriquegermanoalves <carloshenriquecoding023@gmail.com>
2024-01-20 10:33:47 -0300

    Funcao de login

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 //Trabalhando no sistema de login
 
+function login(name, password) {
+    if (name == name) {
+        if (password == password) {
+            return true;
+        }
+    }
+    return false;
+}
+
 //Encerrando o sistema de login
